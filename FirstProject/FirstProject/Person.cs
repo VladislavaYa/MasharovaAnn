f4e1479cb39d91a96129cb0025dd628b39a42fe8
Anna Masharova <Anna_Masharova@epam.com>
2020-10-27 23:29:37 +0200

    addedProject

---
+++ FirstProject/FirstProject/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public int Age
+		{
+			get
+			{
+				return Age;
+			}
+			set
+			{
+				Age = value;
+			}
+		}
+		public void Speak(string s)
+		{
+			string phrase = "fyz";
+			Name = s;
+
+
+			Console.WriteLine (Name +" "+ phrase);
+
+		}
+	}
+}
